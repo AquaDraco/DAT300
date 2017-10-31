16e9b328c47a786c9dbc8354e3fcd5eafb7ec323
BoYuanZheng <aquadracon@gmail.com>
2017-10-31 00:34:37 -0400

    Gets a server up and running with some text

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+var $ = require('jquery');
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
+    fs.readFile('index.html', function(err, data) {
+       res.write(data);       
+    });
 }).listen(1337);
